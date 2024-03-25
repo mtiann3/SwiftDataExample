94b3fd7ceee667c72631d871828afae8e4b560ff
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2024-03-24 20:56:31 -0500

    Initial Commit

--- SwiftDataExample/SwiftDataExampleApp.swift
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataExampleApp: App {
+    
+    let container: ModelContainer = {
+        let schema = Schema([Expense.self])
+        let container = try! ModelContainer(for: schema,configurations: [])
+        return container
+    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(container)
+//        .modelContainer(for: [Expense.self])
     }
 }
